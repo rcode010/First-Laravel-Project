7a486fefbe428186fde114431e3dbe86c744bb05
Raman <ramanazad881@gmail.com>
2025-09-11 00:26:06 +0300

    Model Factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model {
-    protected $table = 'job_listining';
+    use HasFactory;
+    protected $table = 'job_listing';
     protected $fillable = ['title', 'salary'];
 }
--- database/migrations/2025_09_10_205955_job_listing.php
+++ database/migrations/2025_09_10_205955_job_listing.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +12,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('job_listining', function (Blueprint $table) {
+        Schema::create('job_listing', function (Blueprint $table){
             $table->id();
+            // $table->unsignedBigInteger('employer_id');
+            $table->foreignIdFor('App\Models\Employer');
             $table->string('title');
             $table->string('salary');
             $table->timestamps();
@@ -24,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_listining');
+        //
     }
 };
